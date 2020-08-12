2a7b31cc1b4e0109086ea9e15cc45c776dcf415d
aman rathore <aman@kali.aman>
2020-08-12 05:24:30 -0400

    Update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.get('/',function(req,res){
 	res.sendFile(__dirname+'/index.html');
 });
@@ -9,6 +11,6 @@ app.get('/css/style.css',function(req,res){
 	res.sendFile(__dirname+'/css/style.css');
 });
 
-app.listen(3000, function(){
-	console.log('Example app listening on port 3000');
+app.listen(port, function(){
+	console.log('Example app listening on port '+port+' ');
 })
