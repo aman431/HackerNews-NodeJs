2409b001e70ac9adf4392722b529d3201d654bac
aman rathore <aman@kali.aman>
2020-08-12 00:59:56 -0400

    make it better

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',function(req,res){
 	res.sendFile(__dirname+'/index.html');
 });
 
+app.get('/css/style.css',function(req,res){
+	res.sendFile(__dirname+'/css/style.css');
+});
+
 app.listen(3000, function(){
 	console.log('Example app listening on port 3000');
 })
