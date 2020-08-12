bdf0e95261934031eb7e99043fdb7b7d629d4ca0
aman rathore <aman@kali.aman>
2020-08-12 06:52:16 -0400

    Update

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.get('/',function(req,res){
 app.get('/css/style.css',function(req,res){
 	res.sendFile(__dirname+'/css/style.css');
 });
+app.get('/css/Universe.jpeg',function(req,res){
+	res.sendFile(__dirname+'/css/Universe.jpeg');
+});
+app.get('/app.js',function(req,res){
+	res.sendFile(__dirname+'/app.js');
+});
 
 app.listen(port, function(){
 	console.log('Example app listening on port '+port+' ');
